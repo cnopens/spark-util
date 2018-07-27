88b21bdf05d46e2782cf582e2cb171dd28714598
LMQ <LMQ>
2018-07-27 17:20:49 +0800

    update

--- src/main/scala/org/apache/streaming/core/SparkKafkaStreamContext.scala
+++ src/main/scala/org/apache/streaming/core/SparkKafkaStreamContext.scala
@@ -3,7 +3,8 @@ import org.apache.spark.core.SparkKafkaContext
 import org.apache.spark.streaming.Duration
 
 /**
- * 与StreamingKafkaContext不同，这里不使用SparkStreaming来实现流的功能，而是用while true来实现
+ * @author LMQ
+ * @func 与StreamingKafkaContext不同，这里不使用SparkStreaming来实现流的功能，而是用while true来实现
  */
 class SparkKafkaStreamContext {
   var sc: SparkKafkaContext = null
