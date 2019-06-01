4a88c8fbe65265a41685fb771fe658fe1e9f3eaf
LinMQ <Master@LMQ-PC>
2019-06-01 20:31:00 +0800

    Add notes

--- src/main/scala/org/apache/spark/func/tool/package.scala
+++ src/main/scala/org/apache/spark/func/tool/package.scala
@@ -2,6 +2,9 @@ package org.apache.spark.func
 
 import org.apache.spark.streaming.StreamingContext
 import org.apache.spark.rdd.RDD
-
+/**
+ * @author LMQ
+ * @desc 为此package添加隐式转换的function。方便使用，若有其他的隐式功能，可在此package下面添加并，继承
+ */
 package object tool extends KafkaImplicittrait{
 }
