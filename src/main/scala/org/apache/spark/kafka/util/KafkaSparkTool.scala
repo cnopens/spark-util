4112a4be980038088d8ff37326102c2384a2537d
LinMingQiang <1356469429@qq.com>
2019-07-01 18:04:03 +0800

    去掉default_from

--- src/main/scala/org/apache/spark/kafka/util/KafkaSparkTool.scala
+++ src/main/scala/org/apache/spark/kafka/util/KafkaSparkTool.scala
@@ -15,7 +15,6 @@ import org.apache.log4j.PropertyConfigurator
  */
 private[spark] trait KafkaSparkTool extends SparkKafkaConfsKey {
   var logname = "KafkaSparkTool" //外部可重写
-  lazy val defualtFrom: String = "LAST"
   lazy val log = LoggerFactory.getLogger(logname)
   var kp:Map[String, String]
   lazy val kc: KafkaCluster = new KafkaCluster(kp)
